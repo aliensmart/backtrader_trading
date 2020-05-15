729cd6d94543d420aa61d7179b3c79e383000e33
aliensmart <kaoua17@gmail.com>
2020-05-14 23:57:52 -0400

    comments added

--- trade.py
+++ trade.py
@@ -1,13 +1,29 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
+import datetime 
+import os
+
+#Import the backtrader platform
 import backtrader as bt
 
 if __name__ == '__main__':
+    # Create a cerebro entity
     cerebro = bt.Cerebro()
 
+    # Datas are in a subfolder of the samples. Need to find where the script is
+    # because it could have been called from anywhere
+    modpath = os.path.dirname(os.path.abspath(__file__))
+    print(modpath)
+
+    # Set our desired cash start
+    cerebro.broker.setcash(100000.0)
+
+    # Print out the starting conditions
     print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
 
+    # Run over everything
     cerebro.run()
 
+    # Print out the final result
     print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
